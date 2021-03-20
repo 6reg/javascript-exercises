f4e113b2b74fee920ff8b2c3980574e009af162a
greg mathias <mathiasgreg@gmail.com>
2021-03-20 12:10:57 -0400

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,45 @@
-function add () {
-	
+function add (a,b) {
+return a + b 
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b	
 }
 
-function sum () {
-	
+function sum (arr) {
+ let total = arr.reduce((a,b) => {
+	 return a + b
+ }, 0)
+ 	return total 
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	let total = arr.reduce((a,b) => a*b, 1)
+	return total
+
 }
 
-function power() {
-	
+function power(a,b) {
+	let total = a
+	for (let i =  0; i < b; i++) {
+		total *= a;
+		b--;
+	}
+	return total
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if (a == 1 || a == 0) {
+		return 1
+	} else {
+		let total = a;
+		let counter = a
+		for (let i = a; i > 1; i--) {
+			total *= (counter - 1);
+			counter--;
+			}
+		return total
+	}	
 }
 
 module.exports = {
