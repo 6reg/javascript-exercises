585081ae2da58b2f5082ce0414ef7321989c07ab
greg mathias <mathiasgreg@gmail.com>
2021-03-24 19:29:26 -0400

    Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
 
+const palindromes = function(str) { 
+    const cleanStr = str
+                    .toLowerCase()
+                    .replace(/[^A-Za-z]/g, "");
+
+return cleanStr == cleanStr.split("").reverse().join("")
+                    
 }
 
-module.exports = palindromes
+  module.exports = palindromes 
