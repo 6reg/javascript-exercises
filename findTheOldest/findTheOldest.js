2ecca37f20e682ad957f07f23bd43067edaa5d24
greg mathias <mathiasgreg@gmail.com>
2021-04-06 13:38:05 -0400

    FoundTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-let findTheOldest = function() {
+let findTheOldest = function(arr) {
 
-}
+ arr.forEach(p => {
+    if(p.yearOfDeath) {
+        let yearOld = p.yearOfDeath - p.yearOfBirth
+       return p.age = yearOld
+    }
+    let today = new Date().getFullYear()
+    return p.age = today - p.yearOfBirth
+})
+
+arr.sort((a,b) => (a.age < b.age) ? 1 : -1)
+return arr[0]
+
+} 
+        
 
 module.exports = findTheOldest
